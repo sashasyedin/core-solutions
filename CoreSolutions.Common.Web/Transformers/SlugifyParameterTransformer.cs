a23b0db7e35a8cafc4b6b42c326cf9ca1420b293
Alexander Ivanov <sashasyedin@outlook.com>
2019-07-07 22:48:05 +0300

    Add a parameter transformer that allows having routes with hyphens in query parameters name

---
+++ CoreSolutions.Common.Web/Transformers/SlugifyParameterTransformer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System.Text.RegularExpressions;
+
+namespace CoreSolutions.Common.Web.Transformers
+{
+    /// <summary>
+    /// Handles hyphens in routes.
+    /// </summary>
+    public class SlugifyParameterTransformer : IOutboundParameterTransformer
+    {
+        private const string Pattern = "([a-z])([A-Z])";
+        private const string Replacement = "$1-$2";
+
+        /// <summary>
+        /// Transforms the specified route value to a string for inclusion in an URI.
+        /// </summary>
+        /// <param name="value">The route value to transform.</param>
+        /// <returns>
+        /// The transformed value.
+        /// </returns>
+        public string TransformOutbound(object value)
+        {
+            // Slugify value:
+            return value == null
+                ? default
+                : Regex.Replace(value.ToString(), Pattern, Replacement).ToLower();
+        }
+    }
+}
